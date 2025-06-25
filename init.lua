a729e38f08cfbefb8d387ee684c3b9dc1ffe8899
techuila <axlcuyugan05@gmail.com>
2025-06-25 14:38:54 +0800

    feat: add space char

--- init.lua
+++ init.lua
@@ -148,7 +148,12 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = {
+  tab = '» ',
+  trail = '·',
+  space = '·',
+  nbsp = '␣',
+}
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
